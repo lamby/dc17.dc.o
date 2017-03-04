a432d22f42b8fe780c87661474719b4e1e5b6c6f
Stefano Rivera <stefanor@debian.org>
2017-03-04 10:48:18 -0800

    Reword the no bursary option

--- dc17/forms.py
+++ dc17/forms.py
@@ -258,7 +258,7 @@ class BursaryForm(RegistrationFormStep):
     bursary = forms.ChoiceField(
         label='I want to apply for a bursary',
         choices=(
-            ('', 'N/A (not requesting a bursary)'),
+            ('', "No, I'm not equesting a bursary"),
             ('f', 'Food and accommodation only'),
             ('t', 'Food, accommodation *and travel*'),
         ),
