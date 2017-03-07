a6e0a9f2f86ce58177faaa999e0e5710291215fa
Louis-Philippe Véronneau <pollito@riseup.net>
2017-03-07 14:52:25 -0500

    fix alt_accom_choice show/hide

--- dc17/forms.py
+++ dc17/forms.py
@@ -614,7 +614,10 @@ class AccommForm(RegistrationFormStep):
                 'accomm_nights',
                 'accomm_special_requirements',
                 Field('alt_accomm', id='alt_accomm'),
-                Field('alt_accomm_choice', id='alt_accomm_choice'),
+                Fieldset(
+                    '',
+                    'alt_accomm_choice',
+                    css_id='alt_accomm_choice'),
                 css_id='accomm-details',
             ),
             Field('childcare', id='childcare'),
