d85d87a401d6101fe75c11873da72ea7c0a4cdb1
Stefano Rivera <stefanor@debian.org>
2017-03-25 17:25:30 -0400

    Add some bursary review fields

--- dc17/models/bursary.py
+++ dc17/models/bursary.py
@@ -8,6 +8,7 @@ class Bursary(models.Model):
     # unregisters
     user = models.OneToOneField(User, related_name='bursary')
 
+    # Request:
     requesting_travel_bursary = models.BooleanField()
     bursary_reason_contribution = models.TextField()
     bursary_reason_plans = models.TextField()
@@ -15,6 +16,13 @@ class Bursary(models.Model):
     bursary_need = models.CharField(max_length=16)
     travel_bursary = models.IntegerField()
     travel_from = models.TextField()
+
+    # Review:
+    team_notes = models.TextField()
+    rank = models.IntegerField(null=True)
+    approved_accomm = models.NullBooleanField()
+    approved_travel = models.IntegerField(null=True)
+    reimbursed_amount = models.IntegerField(default=0)
 
     def __str__(self):
         return 'Bursary <{}>'.format(self.attendee.user.username)
