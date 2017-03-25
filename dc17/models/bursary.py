d5d2eb408824dc0ce6ccd58e138a9be803abab9e
Stefano Rivera <stefanor@debian.org>
2017-03-25 12:12:24 -0400

    Clean up Bursary model

--- dc17/models/bursary.py
+++ dc17/models/bursary.py
@@ -1,14 +1,18 @@
 from django.db import models
 
+from dc17.models.attendee import Attendee
+
 
 class Bursary(models.Model):
-    bursary = models.CharField(max_length=50)
+    attendee = models.OneToOneField(Attendee, related_name='bursary')
+
+    requesting_travel_bursary = models.BooleanField()
     bursary_reason_contribution = models.TextField()
     bursary_reason_plans = models.TextField()
     bursary_reason_diversity = models.TextField()
-    bursary_need = models.CharField(max_length=50)
+    bursary_need = models.CharField(max_length=16)
     travel_bursary = models.IntegerField()
-    travel_from = models.CharField(max_length=50)
+    travel_from = models.TextField()
 
     def __str__(self):
-        return self.title
+        return 'Bursary <{}>'.format(self.attendee.user.username)
